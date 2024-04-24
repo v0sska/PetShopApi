0a8bf08bb418bebc5c0dc52e414f2ba809ba8d49
v0sska <speedatom80k@gmail.com>
2024-04-24 18:54:39 +0300

    Created a repository

---
+++ src/main/java/com/example/petshopapi/repositories/PetRepository.java
@@ -0,0 +1,12 @@
+package com.example.petshopapi.repositories;
+
+import com.example.petshopapi.entities.Pets;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PetRepository extends CrudRepository<Pets, Long> {
+    List<Pets> searchById(Long id);
+}
